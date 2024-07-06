9c5c2ae7d5b97cf9e128050251d3bd7de052405a
LinikerThiers <linikerthiers@gmail.com>
2024-07-06 09:40:11 -0300

    adicao calculadora de media escolar

--- src/components/contatos.jsx
+++ src/components/contatos.jsx
@@ -9,23 +9,23 @@ const Contatos = ({ isDarkMode }) => {
             <h2 className={`text-2xl font-medium ${isDarkMode ? 'text-gray-300' : 'text-gray-700'} mb-2`}>Entre em </h2>
             <h1 className={`text-4xl font-bold ${isDarkMode ? 'text-white' : 'text-gray-900'} mb-6`}>Contato</h1>
             <div className={`w-auto rounded-lg flex flex-row items-center justify-around border ${isDarkMode ? 'border-gray-400 text-gray-300 bg-gray-800' : 'border-gray-700 text-gray-800 bg-white'}`}>
-            <div>
-                <button className={`px-4 py-2 rounded-lg flex items-center ${isDarkMode ? 'text-gray-300 bg-gray-800 hover:underline' : 'text-gray-800 bg-white hover:underline'}`}>
-                    <FontAwesomeIcon icon={faEnvelope} className="mr-2" />
-                    <a href="mailto:linikerthiers@gmail.com" className='flex flex-row text-lg font-medium'>
-                        linikerthiers@gmail.com
-                    </a>
-                </button>
+                <div>
+                    <button className={`px-4 py-2 rounded-lg flex items-center ${isDarkMode ? 'text-gray-300 bg-gray-800 hover:underline' : 'text-gray-800 bg-white hover:underline'}`}>
+                        <FontAwesomeIcon icon={faEnvelope} className="mr-2" />
+                        <a href="mailto:linikerthiers@gmail.com" className='flex flex-row text-sm md:text-lg font-medium'>
+                            linikerthiers@gmail.com
+                        </a>
+                    </button>
+                </div>
+                <div>
+                    <button className={`px-4 py-2 rounded-lg flex items-center ${isDarkMode ? 'text-gray-300 bg-gray-800 hover:underline' : 'text-gray-800 bg-white hover:underline'}`}>
+                        <FontAwesomeIcon icon={faLinkedin} className="mr-2" />
+                        <a href="https://www.linkedin.com/in/liniker-thiers-a698b01a0/" target="_blank" rel="noopener noreferrer" className='flex flex-row text-sm md:text-lg font-medium'>
+                            LinkedIn
+                        </a>
+                    </button>
+                </div>
             </div>
-            <div>
-                <button className={`px-4 py-2 rounded-lg flex items-center ${isDarkMode ? 'text-gray-300 bg-gray-800 hover:underline' : 'text-gray-800 bg-white hover:underline'}`}>
-                    <FontAwesomeIcon icon={faLinkedin} className="mr-2" /> 
-                    <a href="https://www.linkedin.com/in/liniker-thiers-a698b01a0/" target="_blank" rel="noopener noreferrer" className='flex flex-row text-lg font-medium'>
-                        LinkedIn
-                    </a>
-                </button>
-            </div>
-        </div>
 
         </div>
     );
--- src/components/projetos.jsx
+++ src/components/projetos.jsx
@@ -5,14 +5,15 @@ import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
 import futebolStatistics from '/futebol-statistics.svg'
 import oldEcommerce from '/old-e-commerce.svg'
 import pokedex from '/pokedex.svg'
+import calculadoraEscolar from '/calculadora-de-media-escolar.svg'
 
 
 const Projetos = ({ isDarkMode }) => {
     return (
-        <div className={`w-full flex flex-col items-center justify-center min-h-screen py-8 ${isDarkMode ? 'bg-gray-800' : 'bg-white'}`}>
+        <div className={`w-full flex flex-col items-center justify-center min-h-screen py-8 mt-10 ${isDarkMode ? 'bg-gray-800' : 'bg-white'}`}>
             <h2 className={`text-2xl font-medium ${isDarkMode ? 'text-gray-300' : 'text-gray-700'} mb-2`}>Meus</h2>
             <h1 className={`text-4xl font-bold ${isDarkMode ? 'text-white' : 'text-gray-800'} mb-6`}>Projetos</h1>
-            <div className="w-full flex flex-col md:flex-row items-center md:items-stretch justify-center space-y-4 md:space-y-0 md:space-x-4 px-4 md:px-16 lg:px-32">
+            <div className="w-full flex flex-wrap items-center md:items-stretch justify-center gap-4 space-y-4 md:space-y-0 px-4 md:px-16 lg:px-32">
                 <div className={`w-full md:w-1/3 ${isDarkMode ? 'bg-gray-800 border-white' : 'bg-white border-gray-800'} p-6 rounded-lg border flex flex-col items-center justify-center`}>
                     <h3 className={`text-xl font-medium ${isDarkMode ? 'text-gray-100' : 'text-gray-800'} mb-4 text-center`}>Futebol Statistics</h3>
                     <img
@@ -71,6 +72,22 @@ const Projetos = ({ isDarkMode }) => {
                         <button className={`px-4 py-2 border rounded-lg ${isDarkMode ? 'border-gray-400 text-gray-300 bg-gray-800 hover:bg-gray-700' : 'border-gray-700 text-gray-800 bg-white hover:bg-gray-100'}`}>
                             <a href="https://linikerthiers.github.io/Pokedex/" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center">
                                 <FontAwesomeIcon icon={faExternalLinkAlt} className={`w-5 h-5 ${isDarkMode ? 'text-gray-300' : 'text-gray-800'}`} />
+                            </a>
+                        </button>
+                    </div>
+                </div>
+                {/* Calculadora de media escolar */}
+                <div className={`w-full md:w-1/3 ${isDarkMode ? 'bg-gray-800 border-white' : 'bg-white border-gray-800'} p-6 rounded-lg border flex flex-col items-center justify-center`}>
+                    <h3 className={`text-xl font-medium ${isDarkMode ? 'text-gray-100' : 'text-gray-800'} mb-4 text-center`}>Calculadora de Média Escolar</h3>
+                    <img
+                        alt='Imagem calculadora'
+                        src={calculadoraEscolar}
+                        className='w-full h-auto mb-4 rounded-md'
+                    />
+                    <div className='flex items-center justify-around space-x-4'>
+                        <button className={`px-4 py-2 border rounded-lg ${isDarkMode ? 'border-gray-400 text-gray-300 bg-gray-800 hover:bg-gray-700' : 'border-gray-800 text-gray-800 bg-white hover:bg-gray-100'}`}>
+                            <a href="https://github.com/LinikerThiers/CalculadoraDeMediaEscolar" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center">
+                                <FontAwesomeIcon icon={faGithub} className={`w-5 h-5 ${isDarkMode ? 'text-gray-300' : 'text-gray-800'}`} />
                             </a>
                         </button>
                     </div>
