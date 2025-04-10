7a9f9228a708929f559d66e3205a7f680a027f96
LinikerThiers <linikerthiers@gmail.com>
2025-04-10 16:16:08 -0300

    adicionando o projeto lista de contatos

--- src/components/projetos.jsx
+++ src/components/projetos.jsx
@@ -10,6 +10,7 @@ import contadorPalavras from '/contador-de-palavras.svg'
 import walletWise from '/wallet-wise.svg'
 import calculadoraImc from '/calculadora_imc_pag_1.png'
 import consultaCep from '/tela_inicio_consulta_cep.png'
+import listaDeContatosApp from '/lista-de-contatos-page1.png'
 
 
 const Projetos = ({ isDarkMode }) => {
@@ -34,6 +35,22 @@ const Projetos = ({ isDarkMode }) => {
                         <button className={`px-4 py-2 border rounded-lg ${isDarkMode ? 'border-gray-400 text-gray-300 bg-gray-800 hover:bg-gray-700' : 'border-gray-700 text-gray-800 bg-white hover:bg-gray-100'}`}>
                             <a href="https://frontend-wallet-wise.onrender.com" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center">
                                 <FontAwesomeIcon icon={faExternalLinkAlt} className={`w-5 h-5 ${isDarkMode ? 'text-gray-300' : 'text-gray-800'}`} />
+                            </a>
+                        </button>
+                    </div>
+                </div>
+
+                <div className={`w-full md:w-1/3 ${isDarkMode ? 'bg-gray-800 border-white' : 'bg-white border-gray-800'} p-6 rounded-lg border flex flex-col items-center justify-center`}>
+                    <h3 className={`text-xl font-medium ${isDarkMode ? 'text-gray-100' : 'text-gray-800'} mb-4 text-center`}>Lista de Contatos App</h3>
+                    <img
+                        alt='Lista de Contatos App'
+                        src={listaDeContatosApp}
+                        className='w-48 h-auto mb-4 rounded-md'
+                    />
+                    <div className='flex items-center justify-around space-x-4'>
+                        <button className={`px-4 py-2 border rounded-lg ${isDarkMode ? 'border-gray-400 text-gray-300 bg-gray-800 hover:bg-gray-700' : 'border-gray-800 text-gray-800 bg-white hover:bg-gray-100'}`}>
+                            <a href="https://github.com/LinikerThiers/lista_de_contatos" target="_blank" rel="noopener noreferrer" className="flex items-center justify-center">
+                                <FontAwesomeIcon icon={faGithub} className={`w-5 h-5 ${isDarkMode ? 'text-gray-300' : 'text-gray-800'}`} />
                             </a>
                         </button>
                     </div>
